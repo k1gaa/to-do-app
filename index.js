a4b69b46b47918d2c1cf7f9d006cf410827d7a94
k1gaa <k1gaa@icloud.com>
2024-02-12 22:25:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
+
+const port = 3000 || process.env.port;
+const hostname = "127.0.0.1";
+
+app.listen(port, hostname, () => {
+  console.log(`The server is running on ${hostname}:${port}`);
+});
